7c33683c4a1c387d07fe5794e2e4e6f987658d8b
Sagar-korumilli <93094340+Sagar-korumilli@users.noreply.github.com>
2025-07-15 15:24:10 +0530

    Create V001__hr__full_setup.sql

---
+++ snowflake/setup/V001__hr__full_setup.sql
@@ -0,0 +1,60 @@
+-- V001__hr__full_setup.sql
+-- Full DDL for the HR schema
+
+-- 1. Create schema
+CREATE SCHEMA IF NOT EXISTS hr;
+
+-- 2. Create warehouse
+CREATE WAREHOUSE IF NOT EXISTS hr_wh
+  WITH WAREHOUSE_SIZE = 'XSMALL'
+  AUTO_SUSPEND = 120
+  AUTO_RESUME = TRUE;
+
+-- 3. Role & grants
+CREATE ROLE IF NOT EXISTS hr_analyst;
+GRANT USAGE ON WAREHOUSE hr_wh TO ROLE hr_analyst;
+GRANT USAGE ON DATABASE TPCH TO ROLE hr_analyst;
+GRANT USAGE ON SCHEMA TPCH.hr TO ROLE hr_analyst;
+
+-- 4. Create tables
+CREATE OR REPLACE TABLE hr.employees (
+  employee_id INT,
+  first_name STRING,
+  last_name STRING,
+  hire_date DATE,
+  department STRING
+);
+
+CREATE OR REPLACE TABLE hr.salaries (
+  employee_id INT,
+  salary NUMERIC(10,2),
+  from_date DATE,
+  to_date DATE
+);
+
+-- 5. Create view
+CREATE OR REPLACE VIEW hr.v_current_salaries AS
+SELECT e.employee_id, e.first_name, e.last_name, s.salary
+FROM hr.employees e
+JOIN hr.salaries s
+  ON e.employee_id = s.employee_id
+WHERE s.to_date IS NULL;
+
+-- 6. Sequence
+CREATE OR REPLACE SEQUENCE hr.emp_seq START = 5000 INCREMENT = 1;
+
+-- 7. File format & stage
+CREATE OR REPLACE FILE FORMAT hr.json_ff
+  TYPE = 'JSON';
+
+CREATE OR REPLACE STAGE hr.employee_stage
+  URL = 's3://my-bucket/hr/'
+  FILE_FORMAT = hr.json_ff
+  CREDENTIALS = (AWS_KEY_ID='{{AWS_KEY}}' AWS_SECRET_KEY='{{AWS_SECRET}}');
+
+-- 8. Grants
+GRANT SELECT ON ALL TABLES IN SCHEMA hr TO ROLE hr_analyst;
+GRANT SELECT ON ALL VIEWS IN SCHEMA hr TO ROLE hr_analyst;
+GRANT USAGE ON SEQUENCE hr.emp_seq TO ROLE hr_analyst;
+GRANT USAGE ON FILE FORMAT hr.json_ff TO ROLE hr_analyst;
+GRANT USAGE ON STAGE hr.employee_stage TO ROLE hr_analyst;
