4fa720d06cb96276285649fae31bbfa9df207eb9
Sagar-korumilli <93094340+Sagar-korumilli@users.noreply.github.com>
2025-07-15 22:09:51 +0530

    Update V003__hr__updating_table.sql

--- snowflake/migrations/V003__hr__updating_table.sql
+++ snowflake/migrations/V003__hr__updating_table.sql
@@ -1,10 +1,3 @@
--- V003__hr__add_phone_number_to_employees.sql
--- Adds a PHONE_NUMBER column to the hr.employees table.
-
 ALTER TABLE hr.employees
 ADD COLUMN PHONE_NUMBER VARCHAR(20);
 
--- You can add more statements here if part of the same logical change.
--- For example, updating existing data or granting permissions on the new column:
--- UPDATE hr.employees SET PHONE_NUMBER = 'N/A' WHERE PHONE_NUMBER IS NULL;
--- GRANT SELECT ON COLUMN hr.employees.PHONE_NUMBER TO ROLE hr_analyst;
