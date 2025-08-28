9ac3e2c8f025ff2b7dfdfbd452bd4acebefd646e
unknown <sagar.korumilli11@gmail.com>
2025-08-28 22:47:43 +0530

    done

--- snowflake/hr/V1000__Salaries3.sql
+++
@@ -1,3 +0,0 @@
-CREATE OR REPLACE TABLE Salaraies3 (
-  employee_name Varchar(20)
-  );
--- snowflake/sales/V003__order_items_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TABLE sales.order_items (
-  order_id INT,
-  line_item_id INT,
-  product_id INT,
-  quantity INT,
-  price NUMBER(10,2)
-);
